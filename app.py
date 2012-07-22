d94af8b6783157ae1d3e49ec1a04ef3becc16cb3
George Lewis <ghl227@nyu.edu>
2012-07-22 12:27:00 -0400

    Initial package import, adding skeleton Flask app and empty directories

---
+++ app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+
+import json
+#import simplejson
+
+from flask import Flask
+from flask import url_for
+from flask import render_template
+from flask import request
+from flask import jsonify
+
+import pymongo
+import bson.objectid
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
+    
+
